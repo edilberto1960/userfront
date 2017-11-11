6a07edf73ec42ba70f12ff388cc72cceef80c0ee
HP <HP@192.168.15.2>
2017-11-11 09:33:42 -0600

    extension de comentario

--- userFront/src/main/java/com/userfront/UserFrontApplication.java
+++ userFront/src/main/java/com/userfront/UserFrontApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserFrontApplication {
-	//This is a new line of comments
+	//This is a new line of comments for example
 	public static void main(String[] args) {
 		SpringApplication.run(UserFrontApplication.class, args);
 	}
